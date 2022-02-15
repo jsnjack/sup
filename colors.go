a24063a173c621751b1ed57ba9c930ce9809328f
Yauhen Shulitski <jsnjack@gmail.com>
2022-02-15 16:17:59 +0100

    Add more colours

--- colors.go
+++ colors.go
@@ -3,11 +3,19 @@ package sup
 var (
 	Colors = []string{
 		"\033[32m", // green
-		"\033[33m", // yellow
+		"\033[33m", // orange
 		"\033[36m", // cyan
 		"\033[35m", // magenta
 		"\033[31m", // red
 		"\033[34m", // blue
+		"\033[90m", // dark grey
+		"\033[91m", // light red
+		"\033[92m", // light green
+		"\033[93m", // yellow
+		"\033[94m", // light blue
+		"\033[95m", // light purple
+		"\033[96m", // turquoise
+		"\033[37m", // grey
 	}
 	ResetColor = "\033[0m"
 )
